cbaa81ae971c0b5983af7a52fc3cb382b49f5363
rafiaprilian180402 <mrofii188@gmail.com>
2025-12-18 12:55:37 +0700

    fix auth.js for vercel

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,36 +1,25 @@
-import axios from 'axios'
-import Cookies from 'js-cookie'
+import axios from "axios";
+import Cookies from "js-cookie";
 
-const token = Cookies.get('token')
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Accept: "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    "ngrok-skip-browser-warning": "true", 
+  },
+});
 
-// Dynamic base URL for development vs production
-const getBaseURL = () => {
-    // Development: Use localhost
-   return import.meta.env.VITE_API_URL;
-}
+// Inject token SETIAP request (AMAN production)
+axiosInstance.interceptors.request.use((config) => {
+  const token = Cookies.get("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-axios.defaults.baseURL = getBaseURL()
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-axios.defaults.headers.common['Accept'] = 'application/json'
-if (token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
-
-// Helper function to get full storage URL
-axios.getStorageUrl = (path) => {
-    return `${import.meta.env.VITE_STORAGE_URL}/${path}`;
-}
-
-axios.interceptors.request.use(
-    config => {
-        const token = Cookies.get('token')
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-
-        return config
-    },
-)
-
-
-export const axiosInstance = axios
+// Helper storage URL
+axiosInstance.getStorageUrl = (path) => {
+  return `${import.meta.env.VITE_STORAGE_URL}/${path}`;
+};
